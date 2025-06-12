3e8acad43f4b41f6f3cfa19e043272da3a1c60c4
Ali Fikri <alifikripr@gmail.com>
2025-06-12 09:29:39 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+const Card = ({ title }) => {
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+    </div>
+  );
+};
+const App = () => {
+  return (
+    <div>
+      <h2>Functional Arrow component</h2>
+      <Card title="Avatar" />
+      <Card title="Attack on titans" />
+      <Card title="Fast" />
+    </div>
+  );
+};
+
+export default App;
