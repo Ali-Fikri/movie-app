970638af00f40e75e35724a9c84761e643ceab56
Ali Fikri <alifikripr@gmail.com>
2025-06-12 14:21:43 +0300

    create the heading and start with API

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import Search from "./Components/Search";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = React.useState("");
   return (
     <main>
       <div className="pattern" />
       <div className="wrapper">
         <header>
+          <img src="./hero-img.png" alt="Hero Banner" />
           <h1>
             Find <span className="text-gradient">Movies</span> You'll Love
             Without the Hassle
           </h1>
         </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
   );
