a12fe72df520f26b0a01100a4d6a971b47590325
Ali Fikri <alifikripr@gmail.com>
2025-06-12 10:28:21 +0300

    added the initial styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Love
+            Without the Hassle
+          </h1>
+        </header>
+      </div>
+    </main>
+  );
+};
 
-export default App
+export default App;
